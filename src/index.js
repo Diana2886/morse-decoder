504a3a10183e8050aa4db23719f1ac0f300e2504
diana2886 <tdm2886@gmail.com>
2022-01-28 09:43:17 +0600

    feat: add morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let arrForDecoding = [];
+    
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.substr(i, 10));
+    }
+    for (let item of arr) {
+      let arr1 = [];
+      if (item === '**********') {
+        arrForDecoding.push(' ');
+        continue;
+      } else {
+        for (let i = 0; i < item.length; i += 2) {
+          arr1.push(item.substr(i, 2));
+        }
+      }
+      let arr2 = [];
+      arr1.forEach(e => {
+        if (e === '11') {
+          arr2.push('-');
+        } else if (e === '10') {
+          arr2.push('.');
+        }
+      })
+      arrForDecoding.push(arr2.join(''));
+    }
+    let resultArr = [];
+    for (let i of arrForDecoding) {
+      if (MORSE_TABLE[i]) {
+        resultArr.push(MORSE_TABLE[i]);
+      } else {
+        resultArr.push(' ');
+      }
+    }
+    return resultArr.join('');
 }
 
 module.exports = {
